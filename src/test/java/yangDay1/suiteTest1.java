16f0aacc072d98461321f4e53791919cbbd2c8cf
Tao Lee <281617953@qq.com>
2019-08-07 16:44:55 +0800

    组件测试

---
+++ src/test/java/yangDay1/suiteTest1.java
@@ -0,0 +1,10 @@
+package yangDay1;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StringTest2.class,canshuhua_string_indexof.class})
+public class suiteTest1 {
+}
